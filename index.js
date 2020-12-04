f2cabb9e8e7333b425a83052fcc9e8ad3107fc2a
Alberto Cabrera <alberto.cabrera15@gmail.com>
2020-12-04 21:39:59 +0100

    Fin sección 10 - Auth

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express();
 //Configurar CORS
 app.use( cors() );
 
+//Lectura y parseo del body
+app.use( express.json() );
+
 //Base de datos
 dbConnection();
 
@@ -21,14 +24,8 @@ contraseña: Peta123
 */
 
 //Rutas
-app.get( '/', (req, res) => {
-
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-
-});
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
 
 app.listen( process.env.PORT, () => {
